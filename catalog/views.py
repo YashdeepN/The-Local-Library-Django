23c5e319473e46ccb5815f3ffb9adc2348197b66
YashdeepN <yashdeepneema@hotmail.com>
2024-12-09 23:40:09 -0600

    create library home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-
+from .models import Book, BookInstance, Author, Genre
 from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("index of catalog")
+    """View funtion for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by dafault..
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
