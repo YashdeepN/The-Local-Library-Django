9faa21ecb95d257d38b1748c28ef5094cfe43ffb
YashdeepN <yashdeepneema@hotmail.com>
2024-12-12 21:52:46 -0600

    show number of visiters using sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,11 @@ def index(request):
     # The 'all()' is implied by dafault..
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     # Nos. of genres and books that contaion a particular word:
 
     context = {
@@ -26,6 +31,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
+
     }
 
     return render(request, 'index.html', context=context)
