1b22d8728423f49997ebf4ef846bf074d08ca36c
YashdeepN <yashdeepneema@hotmail.com>
2024-12-15 21:31:44 -0600

    user authentication and permissions- show list of books borrowd by a user

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+from django.conf import settings
+from datetime import date
 import uuid
 
 # Create your models here.
@@ -119,8 +121,21 @@ class BookInstance(models.Model):
         help_text='Book Availability',
     )
 
+    borrower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
